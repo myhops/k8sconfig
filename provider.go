b3897c6ad00e256312ce9ebe774ac61341c721db
Peter Zandbergen <peter.zandbergen@myhops.com>
2025-08-08 16:37:31 +0200

    change scheme and add uri parsing plus tests

--- provider.go
+++ provider.go
@@ -3,17 +3,27 @@ package k8sconfig
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"strings"
 
 	"go.opentelemetry.io/collector/confmap"
 	"go.uber.org/zap"
 )
 
-const schemeName = "k8sconfig"
+const schemeName = "k8scfg"
 
 type provider struct {
 	// Add k8s credentials
 	logger *zap.Logger
+}
+
+// valueSpec contains the result of the parseURI
+type valueSpec struct {
+	kind string
+	namespace string
+	name string
+	dataType string
+	key string
 }
 
 func NewFactory() confmap.ProviderFactory {
@@ -26,6 +36,39 @@ func newProvider(settings confmap.ProviderSettings) confmap.Provider {
 	}
 }
 
+func parseURI(uri string) (*valueSpec, error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	// Test scheme
+	if u.Scheme != schemeName {
+		return nil, fmt.Errorf("expected k8scfg scheme")
+	}
+	// Opaque should be set
+	if u.Opaque == "" {
+		return nil, fmt.Errorf("not opaque")
+	}
+
+	parts := strings.Split(u.Opaque, "/")
+	if len(parts) != 5 {
+		return nil, fmt.Errorf("need 5 parts, got %d", len(u.Opaque))
+	}
+
+	return &valueSpec{
+		kind: parts[0],
+		namespace: parts[1],
+		name: parts[2],
+		dataType: parts[3],
+		key: parts[4],
+	}, nil
+}
+
+func (v *valueSpec) validate() error {
+	return nil
+}
+
 func getFromConfigMap(ctx context.Context, namespace string, name string, field string, item string) ([]byte, error) {
 	// first check the field type
 	if field != "data" && field != "binaryData" {
@@ -35,14 +78,14 @@ func getFromConfigMap(ctx context.Context, namespace string, name string, field 
 	cm, err := getConfigMap(ctx, namespace, name)
 	if err != nil {
 		return nil, err
-	}	
-	
+	}
+
 	var res []byte
 	switch field {
-		case "data":
-			res = []byte(cm.Data[item])
-		case "binaryData":
-			res = cm.BinaryData[item]
+	case "data":
+		res = []byte(cm.Data[item])
+	case "binaryData":
+		res = cm.BinaryData[item]
 	}
 	if len(res) == 0 {
 		return nil, fmt.Errorf("%s[%s] is empty", field, item)
@@ -56,17 +99,17 @@ func getFromSecret(ctx context.Context, namespace string, name string, field str
 		return nil, fmt.Errorf("field must be either data or binaryData")
 	}
 
-s, err := getSecret(ctx, namespace, name)
+	s, err := getSecret(ctx, namespace, name)
 	if err != nil {
 		return nil, err
-	}	
-	
+	}
+
 	var res []byte
 	switch field {
-		case "data":
-			res = s.Data[item]
-		case "binaryData":
-			res = []byte(s.StringData[item])
+	case "data":
+		res = s.Data[item]
+	case "binaryData":
+		res = []byte(s.StringData[item])
 	}
 	if len(res) == 0 {
 		return nil, fmt.Errorf("%s[%s] is empty", field, item)
@@ -75,12 +118,15 @@ s, err := getSecret(ctx, namespace, name)
 }
 
 // Retreive gets the data from either a secret or a config map using the following schema
+//
 //	k8sconfig:configMap:namespace:<configmap name>:<data or binaryData>:<item>  for a configmap
 //	k8sconfig:secret:namespace:<secret name>:<data or stringData>:<item>  for a secret
 func (p *provider) Retrieve(ctx context.Context, uri string, _ confmap.WatcherFunc) (*confmap.Retrieved, error) {
 
+	// use uri to parse
+
 	parts := strings.Split(uri, ":")
-	if l := len(parts ); l != 6 {
+	if l := len(parts); l != 6 {
 		return nil, fmt.Errorf("number of parts must be 6, got %v", l)
 	}
 
@@ -92,9 +138,9 @@ func (p *provider) Retrieve(ctx context.Context, uri string, _ confmap.WatcherFu
 	var err error
 	switch strings.ToLower(parts[1]) {
 	case "configmap":
-			res, err = getFromConfigMap(ctx, parts[2],parts[3], parts[4], parts[5])
+		res, err = getFromConfigMap(ctx, parts[2], parts[3], parts[4], parts[5])
 	case "secret":
-		res, err = getFromSecret(ctx, parts[2],parts[3], parts[4], parts[5])
+		res, err = getFromSecret(ctx, parts[2], parts[3], parts[4], parts[5])
 	}
 	if err != nil {
 		return nil, err
---
+++ provider_test.go
@@ -0,0 +1,74 @@
+package k8sconfig
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseURI(t *testing.T) {
+	type args struct {
+		uri string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *valueSpec
+		wantErr bool
+	}{
+		{
+			name: "valid uri",
+			args: args{
+				uri: "k8scfg:secret/ns1/secret1/data/key",
+			},
+			want: &valueSpec{
+				kind:      "secret",
+				namespace: "ns1",
+				name:      "secret1",
+				dataType:  "data",
+				key:       "key",
+			},
+			wantErr: false,
+		},
+		{
+			name: "not opaque uri",
+			args: args{
+				uri: "k8scfg://secret/ns1/secret1/data/key",
+			},
+			wantErr: true,
+		},
+		{
+			name: "bad scheme",
+			args: args{
+				uri: "k8scfg1://secret/ns1/secret1/data/key",
+			},
+			wantErr: true,
+		},
+		{
+			name: "too few parts",
+			args: args{
+				uri: "k8scfg:secret/ns1/secret1/data",
+			},
+			wantErr: true,
+		},
+		{
+			name: "too many parts",
+			args: args{
+				uri: "k8scfg:secret/ns1/secret1/data/key/tomany",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseURI(tt.args.uri)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseURI() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseURI() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
